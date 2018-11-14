f65bb2398462a5323fa2610508a510ad514ce740
Oğuzhan Tuna ÖZDOĞAN <han5-1@hotmail.com>
2018-11-14 13:16:03 +0300

    /movies/between endpoint

---
+++ helper/dbs.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://movie_user:abcd1234@ds039231.mlab.com:39231/movie-api', { useMongoClient: true });
+    
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected')
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+};
